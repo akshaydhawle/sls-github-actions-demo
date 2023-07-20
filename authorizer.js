cd8e8b7455b4ca2841832cb27103f8924fa9cb25
Akshaykumar Dhawle <akshaykumar.dhawle@treez.io>
2023-07-20 13:56:23 +0530

    added github configurations

--- authorizer.js
+++ authorizer.js
@@ -1,46 +1,43 @@
-const { CognitoJwtVerifier } = require("aws-jwt-verify");
-const COGNITO_USERPOOL_ID = process.env.COGNITO_USERPOOL_ID;
+const { CognitoJwtVerifier } = require('aws-jwt-verify');
+
+const COGNITO_USER_POOL_ID = process.env.COGNITO_USER_POOL_ID;
 const COGNITO_WEB_CLIENT_ID = process.env.COGNITO_WEB_CLIENT_ID;
 
 const jwtVerifier = CognitoJwtVerifier.create({
-  userPoolId: COGNITO_USERPOOL_ID,
-  tokenUse: "id",
-  clientId: COGNITO_WEB_CLIENT_ID
+    userPoolId: COGNITO_USER_POOL_ID,
+    tokenUse: "id",
+    clientId: COGNITO_WEB_CLIENT_ID
 })
 
 const generatePolicy = (principalId, effect, resource) => {
-  var authReponse = {};
-  authReponse.principalId = principalId;
-  if (effect && resource) {
-    let policyDocument = {
-      Version: "2012-10-17",
-      Statement: [
-        {
-          Effect: effect,
-          Resource: resource,
-          Action: "execute-api:Invoke",
+    return {
+        principalId,
+        policyDocument: {
+            Version: '2012-10-17',
+            Statement: [
+                {
+                    Action: 'execute-api:Invoke',
+                    Effect: effect,
+                    Resource: resource,
+                },
+            ],
         },
-      ],
+        context: {
+            name: "akshay"
+        }
     };
-    authReponse.policyDocument = policyDocument;
-  }
-  authReponse.context = {
-    foo: "bar",
-  };
-  console.log(JSON.stringify(authReponse));
-  return authReponse;
 };
 
-exports.handler = async (event, context, callback) => {
-  // lambda authorizer code
-  var token = event.authorizationToken;
-  console.log(token);
-  // Validate the token
-  try {
-    const payload = await jwtVerifier.verify(token);
-    console.log(JSON.stringify(payload));
-    callback(null, generatePolicy("user", "Allow", event.methodArn));
-  } catch(err) {
-    callback("Error: Invalid token");
-  }
-};
+module.exports.handler = async (event) => {
+    // we have two modes request and token, token is default 
+    const token = event.authorizationToken; // allow or deny
+    console.log(token);
+
+    try {
+        const payload = await jwtVerifier.verify(token);
+        console.log(JSON.stringify(payload));
+        return generatePolicy('user', 'Allow', event.methodArn);
+    } catch (error) {
+        return "Invalid Token";
+    }
+}
--- handler.js
+++ handler.js
@@ -1,81 +1,118 @@
-"use strict";
-const DynamoDB = require("aws-sdk/clients/dynamodb");
-const documentClient = new DynamoDB.DocumentClient({ region: "us-east-1" });
-const NOTES_TABLE_NAME = process.env.NOTES_TABLE_NAME;
+'use strict';
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const {
+  DynamoDBDocumentClient,
+  PutCommand,
+  UpdateCommand,
+  DeleteCommand,
+  ScanCommand,
+} = require('@aws-sdk/lib-dynamodb');
 
-const send = (statusCode, data) => {
-  return {
-    statusCode,
-    body: JSON.stringify(data),
-  };
-};
-module.exports.createNote = async (event, context, cb) => {
-  let data = JSON.parse(event.body);
+const client = new DynamoDBClient({  region: process.env.AWS_REGION})
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+const notesTableName = process.env.NOTES_TABLE_NAME;
+
+module.exports.createNote = async (event,context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const data = JSON.parse(event.body);
   try {
     const params = {
-      TableName: NOTES_TABLE_NAME,
+      TableName: notesTableName,
       Item: {
         notesId: data.id,
         title: data.title,
-        body: data.body,
+        body: data.body
       },
-      ConditionExpression: "attribute_not_exists(notesId)",
+      ConditionExpression: "attribute_not_exists(notesId)"
+    }
+    await ddbDocClient.send(new PutCommand(params))
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        message:'new note created',
+        data
+      }),
     };
-    await documentClient.put(params).promise();
-    cb(null, send(201, data));
-  } catch (err) {
-    cb(null, send(500, err.message));
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    };
   }
 };
 
-module.exports.updateNote = async (event, context, cb) => {
-  let notesId = event.pathParameters.id;
-  let data = JSON.parse(event.body);
+module.exports.updateNote = async (event) => {
+  const notesId = event.pathParameters.id;
+  const data = JSON.parse(event.body);
   try {
     const params = {
-      TableName: NOTES_TABLE_NAME,
+      TableName: notesTableName,
       Key: { notesId },
-      UpdateExpression: "set #title = :title, #body = :body",
+      UpdateExpression: 'set #title = :title, #body = :body',
       ExpressionAttributeNames: {
-        "#title": "title",
-        "#body": "body",
+        "#title":"title",
+        "#body":"body"
       },
       ExpressionAttributeValues: {
         ":title": data.title,
-        ":body": data.body,
+        ":body": data.body
       },
-      ConditionExpression: "attribute_exists(notesId)",
+      ConditionExpression: 'attribute_exists(notesId)',
+      ReturnValues: 'ALL_NEW'
+    }
+    const response = await ddbDocClient.send(new UpdateCommand(params))
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: `note with id: ${notesId} updated!`,
+        data: response
+      }),
     };
-    await documentClient.update(params).promise();
-    cb(null, send(200, data));
-  } catch (err) {
-    cb(null, send(500, err.message));
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    };
+  }
+};
+module.exports.deleteNote = async (event) => {
+  const notesId = event.pathParameters.id;
+  try {
+    const params = {
+      TableName: notesTableName,
+      Key : { notesId },
+      ConditionExpression: 'attribute_exists(notesId)',
+    }
+    await ddbDocClient.send(new DeleteCommand(params))
+    return {
+      statusCode: 200,
+      body: JSON.stringify("Item deleted Successfully")
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message)
+    }
+  }
+};
+module.exports.getAllNotes = async (event) => {
+  // context information from authorizer
+  console.log(event.requestContext.authorizer);
+  try {
+    let params = {
+      TableName: notesTableName
+    };
+    const response = await ddbDocClient.send(new ScanCommand(params))
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.Items),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    };
   }
 };
 
-module.exports.deleteNote = async (event, context, cb) => {
-  let notesId = event.pathParameters.id;
-  try {
-    const params = {
-      TableName: NOTES_TABLE_NAME,
-      Key: { notesId },
-      ConditionExpression: "attribute_exists(notesId)",
-    };
-    await documentClient.delete(params).promise();
-    cb(null, send(200, notesId));
-  } catch (err) {
-    cb(null, send(500, err.message));
-  }
-};
-
-module.exports.getAllNotes = async (event, context, cb) => {
-  try {
-    const params = {
-      TableName: NOTES_TABLE_NAME,
-    };
-    const notes = await documentClient.scan(params).promise();
-    cb(null, send(200, notes));
-  } catch (err) {
-    cb(null, send(500, err.message));
-  }
-};
